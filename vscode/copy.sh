4f6a526f3cb9ff2fe56a507a95438f26abc75163
ryuta69 <eyma22s.yu@gmail.com>
2020-02-03 02:42:52 +0900

    update vscode plugins

--- vscode/copy.sh
+++ vscode/copy.sh
@@ -1,5 +1,5 @@
 export DOTPATH=~/.config/dotfiles
 cp -r ~/Library/Application\ Support/Code/User/snippets ${DOTPATH}/vscode/ &&
 	cp ~/Library/Application\ Support/Code/User/settings.json ${DOTPATH}/vscode/ &&
-	code --list-extensions | xargs -L 1 echo code --install-extension > ${DOTPATH}/vscode/plugins.txt &&
+	code --list-extensions > ${DOTPATH}/vscode/plugins.txt &&
 	cp ~/Library/Application\ Support/Code/User/keybindings.json ${DOTPATH}/vscode/
