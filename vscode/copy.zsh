adddfac252b406d3f521d8705d32b5f9b84b464c
ryuta69 <eyma22s.yu@gmail.com>
2020-02-18 19:19:07 +0900

    update vscode setting

--- vscode/copy.zsh
+++ vscode/copy.zsh
@@ -1,5 +1,5 @@
 export DOTPATH=~/.ghq/github.com/ryuta69/dotfiles
-cp -r ~/Library/Application\ Support/Code/User/snippets ${DOTPATH}/vscode/ &&
-	cp ~/Library/Application\ Support/Code/User/settings.json ${DOTPATH}/vscode/ &&
+
+cp ~/Library/Application\ Support/Code/User/settings.json ${DOTPATH}/vscode/ &&
 	code --list-extensions > ${DOTPATH}/vscode/plugins.txt &&
 	cp ~/Library/Application\ Support/Code/User/keybindings.json ${DOTPATH}/vscode/
---
+++ vscode/install.zsh
@@ -0,0 +1,4 @@
+code --install-extension $(cat Npmfile)
+
+ln -sfnv ~/.ghq/github.com/ryuta69/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sfnv ~/.ghq/github.com/ryuta69/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
