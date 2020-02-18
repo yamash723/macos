4955110863ebcee9dac8ba38e8fc1d2705ce6c8e
ryuta69 <eyma22s.yu@gmail.com>
2020-02-18 22:30:12 +0900

    update vscode copy.zsh

--- vscode/copy.zsh
+++ vscode/copy.zsh
@@ -1,8 +1,9 @@
 #! /usr/local/bin/zsh
 
 DOTPATH="~/.ghq/github.com/ryuta69/dotfiles/vscode"
-VSPATH="~/Library/Application\ Support/Code/User"
+if [[ $PWD != ${DOTPATH} ]]; then echo 'Must execute in '${DOTPATH} && exit; fi;
 
+VSPATH="~/Library/Application\ Support/Code/User"
 cp ${VSPATH}/settings.json ${DOTPATH} &&
 	code --list-extensions > ${DOTPATH}/plugins.txt &&
 	cp ${VSPATH}/keybindings.json ${DOTPATH}
