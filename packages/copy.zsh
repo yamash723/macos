123a6ecff80f0752ef28d54ea59fcc55866b5358
ryuta69 <eyma22s.yu@gmail.com>
2020-02-24 13:44:37 +0900

    Fix(Npmfile): Remove version by awk
    Global library is fine with latest.

--- packages/copy.zsh
+++ packages/copy.zsh
@@ -1,4 +1,4 @@
 #! /usr/local/bin/zsh
 
 EXEPATH=$0:A:h
-npm list -g --depth 0 | sed '1d' | awk '{ print $2 }' > ${EXEPATH}/Npmfile
+npm list -g --depth 0 | sed '1d' | awk '{ print $2 }' | awk -F'@[0-9]' '{ print $1 }' > ${EXEPATH}/Npmfile
