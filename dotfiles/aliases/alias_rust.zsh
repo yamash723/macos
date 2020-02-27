f0960d18a2021cbc8f579960e5107a3ae06403b6
ryuta69 <eyma22s.yu@gmail.com>
2020-02-27 09:33:50 +0900

    Add(Rust): Add Rust Plugins and Alias
    Rust and Rustup are installed by brew.

---
+++ dotfiles/aliases/alias_rust.zsh
@@ -0,0 +1,8 @@
+function cnew() {
+	cargo new $1 --bin;
+}
+alias cbl='cargo build'
+alias cre='cargo release'
+alias crn='cargo run'
+alias cck='cargo check:w
+'
--- vscode/copy.zsh
+++ vscode/copy.zsh
@@ -2,6 +2,4 @@
 
 EXEPATH=$0:A:h
 VSPATH="~/Library/Application\ Support/Code/User"
-cp ${VSPATH}/settings.json ${EXEPATH}
-cp ${VSPATH}/keybindings.json ${EXEPATH}
-code --list-extensions > ${EXEPATH}/plugins.txt
+code --list-extensions > ${EXEPATH}/Pluginfile
