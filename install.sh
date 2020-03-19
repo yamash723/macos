42e82c46b1dc6de3dba213fbf239266458aa429d
ryuta69 <eyma22s.yu@gmail.com>
2020-03-19 09:47:02 +0900

    Refactor(Makefile): Leave references about Makefile
    It was used to be made with Makefile.

--- install.sh
+++ install.sh
@@ -75,3 +75,48 @@ do
 		;;
 	esac
 done
+
+## ----------------------------------------
+##	Makefile
+##	- This initialize was used to be made with Makefile.
+##	- However, I noticed that make doesn't exist in default OS.
+##	- I leave code here ust for reference.
+## ----------------------------------------
+
+# help:
+# 	@echo "\nᓚᘏᗢ < This is my personal dotfiles.\n"
+# 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "%-10s: %s\n", $$1, $$2}'
+
+# init: ## install dependencies.
+# 	mkdir -p ~/.ssh
+# 	mkdir -p ~/work
+# 	mkdir -p ~/.config/nvim
+# 	xcode-select --install
+# 	ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+# 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# 	brew install zsh
+# 	sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
+# 	sudo chsh -s /usr/local/bin/zsh
+# 	chmod 755 /usr/local/share/zsh
+# 	chmod 755 /usr/local/share/zsh/site-functions
+# 	brew tap homebrew/bundle
+# 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# packages: ## install package manager files.
+# 	./packages/install.zsh
+
+# dotfiles: ## install dotfiles.
+# 	./dotfiles/install.zsh
+
+# vscode: ## install VSCode settings.
+# 	./vscode/install.zsh
+
+# macos: ## install MacOS settings.
+# 	./macos/install.zsh
+
+# all: ## install all settings.
+# 	@echo "Your file will be overwritten(Y/n)" && read ans && [ $${ans:-N} = Y ]
+# 	./packages/install.zsh --force
+# 	./dotfiles/install.zsh --force
+# 	./vscode/install.zsh --force
+# 	./macos/install.zsh --force
