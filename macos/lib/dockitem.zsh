ec087e768a912a4bfea3dadf8a40f75e9c24bccd
ryuta69 <eyma22s.yu@gmail.com>
2020-03-20 01:47:17 +0900

    Refactor(DockItem): Change applescript to plistbuddy and zsh
    It's cleaner.

---
+++ macos/lib/dockitem.zsh
@@ -0,0 +1,57 @@
+#! /usr/local/bin/zsh
+
+defaults delete com.apple.dock persistent-apps
+
+dockitem=(
+	"Mail"                "com.apple.mail"                            "file:///Applications/Mail.app/"
+	"Docker"              "com.docker.docker"                         "file:///Applications/Docker.app/"
+	"App Store"           "com.apple.AppStore"                        "file:///Applications/App%20Store.app"
+	"Xcode"               "com.apple.dt.Xcode"                        "file:///Applications/Xcode.app/"
+	"Kindle"              "com.amazon.Kindle"                         "file:///Applications/Kindle.app/"
+	"Visual Studio Code"  "com.microsoft.VSCode"                      "file:///Applications/Visual%20Studio%20Code.app"
+	"zoom.us"             "us.zoom.xos"                               "file:///Applications/zoom.us.app/"
+	"Discord"             "com.hnc.Discord"                           "file:///Applications/Discord.app/"
+	"Transmit"            "com.panic.Transmit"                        "file:///Applications/Transmit.app/"
+	"VirtualBox"          "org.virtualbox.app.VirtualBox"             "file:///Applications/VirtualBox.app/"
+	"QuickTime Player"    "com.apple.QuickTimePlayerX"                "file:///Applications/QuickTime%20Player.app"
+	"Evernote"            "com.evernote.Evernote"                     "file:///Applications/Evernote.app/"
+	"MongoDB Compass"     "com.mongodb.compas"                        "file:///Applications/MongoDB%20Compass.app"
+	"Android Studio"      "com.google.android.studio"                 "file:///Applications/Android%20Studio.app"
+	"LINE"                "jp.naver.line.mac"                         "file:///Applications/LINE.app/"
+	"Local"               "com.getflywheel.lightning.local"           "file:///Applications/Local.app/"
+	"Sequel Pro"          "com.sequelpro.SequelPro"                   "file:///Applications/Sequel%20Pro.app"
+	"Slack"               "com.tinyspeck.slackmacgap"                 "file:///Applications/Slack.app/"
+	"Calendar"            "com.apple.iCal"                            "file:///Applications/Calendar.app/"
+	"Postman"             "com.postmanlabs.mac"                       "file:///Applications/Postman.app/"
+	"Google Chrome"       "com.google.Chrome"                         "file:///Applications/Google%20Chrome.app"
+	"Adobe XD"            "com.adobe.xd"                              "file:///Applications/Adobe%20XD/Adobe%20XD.app"
+	"Voice Memos"         "com.apple.VoiceMemos"                      "file:///Applications/VoiceMemos.app/"
+	"Gifski"              "com.sindresorhus.Gifski"                   "file:///Applications/Gifski.app/"
+	"Alfred Preferences"  "com.runningwithcrayons.Alfred-Preferences" "file:///Applications/Alfred%204.app/Contents/Preferences/Alfred%20Preferences.app/"
+	"iTerm"               "com.googlecode.iterm2"                     "file:///Applications/iTerm.app/"
+	"Automator"           "com.apple.Automator"                       "file:///Applications/Automator.app/"
+	"Digital Color Meter" "com.apple.DigitalColorMeter"               "file:///Applications/Utilities/Digital%20Color%20Meter.app"
+	"System Preferences"  "com.apple.systempreferences"               "file:///Applications/System%20Preferences.app"
+)
+
+PLIST="${HOME}/Library/Preferences/com.apple.dock.plist"
+/usr/libexec/PlistBuddy -c "Add persistent-apps array" ${PLIST}
+
+DNUM=$(expr ${dockitem[(I)$dockitem[-1]]} / 3)
+foreach idx ($(seq 0 $(expr ${DNUM} - 1)))
+	NAMEIDX=${dockitem[$(( ${idx} * 3 + 1 ))]}
+	ITEMIDX=${dockitem[$(( ${idx} * 3 + 2 ))]}
+	PATHIDX=${dockitem[$(( ${idx} * 3 + 3 ))]}
+
+	/usr/libexec/PlistBuddy \
+		-c "Add persistent-apps:${idx} dict" \
+		-c "Add persistent-apps:${idx}:tile-data dict" \
+		-c "Add persistent-apps:${idx}:tile-data:file-label string ${NAMEIDX}" \
+		-c "Add persistent-apps:${idx}:tile-data:bundle-identifier string ${ITEMIDX}" \
+		-c "Add persistent-apps:${idx}:tile-data:file-data dict" \
+		-c "Add persistent-apps:${idx}:tile-data:file-data:_CFURLString string ${PATHIDX}" \
+		-c "Add persistent-apps:${idx}:tile-data:file-data:_CFURLStringType integer 15" \
+		${PLIST}
+end
+
+killall Dock
