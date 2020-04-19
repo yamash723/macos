b152bf57eea29028f186244dc8cdd47867fe1582
ryuta69 <eyma22s.yu@gmail.com>
2020-04-19 10:19:15 +0900

    Add(VSCode): Setting up ESLint and Prettier

--- dotfiles/.node_template/.eslintrc.js
+++ dotfiles/.node_template/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-	extends: [
-		'plugin:prettier/recommended'
-	]
-}
+	"extends": [
+		"plugin:prettier/recommended"
+	],
+	"plugins": ["prettier"],
+	rules: {
+		"prettier/prettier": "error",
+		comma-dangle: [ "error", "consistent" ],
+		array-element-newline : ["error", {"multiline": true}]
+	},
+};
