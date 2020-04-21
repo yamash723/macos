2cfcdfa3a30fb04b34255de688ad8a1de56dd75f
ryuta69 <eyma22s.yu@gmail.com>
2020-04-21 19:21:05 +0900

    Add(Vsnip): Replace alfred snippets in vsnip

--- dotfiles/.node_template/.eslintrc.js
+++ dotfiles/.node_template/.eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
+	"env": {
+		"es6": true,
+		"node": true
+	},
 	"extends": [
+		"eslint:recommended",
+		"standard",
 		"plugin:prettier/recommended"
 	],
 	"plugins": ["prettier"],
 	rules: {
-		"prettier/prettier": "error",
+		"prettier/prettier": [
+			"error", {
+				"arrowParens": "always",
+				"bracketSpacing": true,
+				"printWidth": 100,
+				"semi": false,
+				"singleQuote": true
+			}
+		],
 		comma-dangle: [ "error", "consistent" ],
 		array-element-newline : ["error", {"multiline": true}]
 	},
