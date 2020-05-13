9e9431d09793493e9abd7eb0c3292ca747330ad6
ryuta69 <eyma22s.yu@gmail.com>
2020-05-13 21:19:41 +0900

    Add(as-tree): Installation and Global alias

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ install_bundle() {
 	rustup component add rust-src --toolchain stable
 	rustup component add rls-preview --toolchain stable
 	rustup component add rust-analysis --toolchain stable
+	cargo install -f --git https://github.com/jez/as-tree
 
 	## ========== Perl ==========
 	cpanm $(cat ${CWD}/Cpanfile)
