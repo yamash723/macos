e95c6836ac284ad493c7564be3533fdbb0e9bc5f
ryuta69 <eyma22s.yu@gmail.com>
2020-06-30 14:04:42 +0900

    Add pip updater alias

--- dotfiles/.aliases/base.zsh
+++ dotfiles/.aliases/base.zsh
@@ -1,11 +1,12 @@
 ## ----------------------------------------
 ##	Package Manager
 ## ----------------------------------------
-alias rsup='cargo install-update --all'
 alias zsup='zinit self-update && zinit update'
 alias npup='npm update -g npm && npm update -g'
+alias rsup='rustup update && cargo install-update --all'
 alias viup='vim +"PlugUpdate" +qa && nvim +"PlugUpdate" +qa'
 alias brup='brew upgrade && brew update && brew cask upgrade'
+alias piup='pip install --upgrade pip && pip freeze | cut -d'=' -f1 | xargs -n1 pip install --upgrade'
 
 ## ----------------------------------------
 ##	ssh
