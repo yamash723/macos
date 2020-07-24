844609c2e7d7829f1f41d691a4dbea1377efa5dd
ryuta69 <eyma22s.yu@gmail.com>
2020-07-24 23:49:32 +0900

    Add hyper config

---
+++ dotfiles/.hyper.js
@@ -0,0 +1,29 @@
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 12,
+    fontFamily: 'HackNerdFontComplete-Italic',
+    fontWeight: 'normal',
+    fontWeightBold: 'bold',
+    lineHeight: 1,
+    letterSpacing: 0,
+    cursorShape: 'BLOCK',
+    cursorBlink: false,
+    padding: '12px 14px',
+    shellArgs: ['--login'],
+    bell: 'false',
+    copyOnSelect: true,
+    defaultSSHApp: true,
+    quickEdit: false,
+    macOptionSelectionMode: 'vertical',
+    webGLRenderer: true,
+	pokemon: 'random',
+    unibody: 'true',
+    poketab: 'true',
+    pokemonSyntax: 'dark',
+    pokecursor: 'true'
+  },
+  plugins: [
+	'hyper-pokemon'
+  ],
+};
