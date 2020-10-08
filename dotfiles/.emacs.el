e3265c2af108347dfc0ab1bc2dfd75519ba48a61
ryuta69 <eyma22s.yu@gmail.com>
2020-10-08 12:10:50 +0900

    Add emacs config mostly overwritten for vim keybind

---
+++ dotfiles/.emacs.el
@@ -0,0 +1,30 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'evil) (package-install 'evil))
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(show-paren-mode 1)
+(global-linum-mode t)
+(electric-pair-mode 1)
+(setq inhibit-startup-message t)
+(prefer-coding-system 'utf-8)
+(set-language-environment  'utf-8)
+(setq-default tab-width 4 indent-tabs-mode nil)
